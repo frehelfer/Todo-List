c0844b840b96eab4409457197bf1cc5e79f25e6b
frehelfer <fre.helfer@gmail.com>
2022-10-14 17:45:08 -0300

    Initial Commit

---
+++ Todo-List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo-List
+//
+//  Created by Frédéric Helfer on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
