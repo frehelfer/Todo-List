dc0bc7bc4f133f5942d69bdd286fafe7073d9f07
frehelfer <fre.helfer@gmail.com>
2022-10-14 21:02:53 -0300

    Added var details to Model

--- Todo-List/Model/ItemModel.swift
+++ Todo-List/Model/ItemModel.swift
@@ -10,7 +10,8 @@ import Foundation
 struct ItemModel: Identifiable, Codable, Equatable {
     var id = UUID()
     var title: String
+    var details: String
     var done = false
     
-    static var example = ItemModel(title: "Caminhar com a Besh")
+    static var example = ItemModel(title: "Caminhar com a Besh", details: "De noite")
 }
